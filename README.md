a9449bb2aab613e2ae431d8e2253cef1e02fe436
IgorDGomes <igordgomes@gmail.com>
2024-04-03 12:48:18 +0100

    Aula 1, 2: criação da interface

---
+++ README.md
@@ -0,0 +1,4 @@
+<!-- 
+    Follow `https://www.nativewind.dev/v4/getting-started/expo-router` instructions to set the nativewind
+    npx install expo-font @expo-google-fonts/roboto
+-->
---
+++ src/app/index.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { View, Image, StatusBar, Alert } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons"
+import { Link } from "expo-router";
+
+import { colors } from "@/styles/colors";
+
+import { Input } from "@/components/input";
+import { Button } from "@/components/button";
+
+
+export default function Home() {
+    const [code, setCode] = useState('')
+
+    function handleAccessCredential() {
+        if (!code.trim()) {
+            return Alert.alert("Ingresso", "Informe o código do ingresso!")
+        }
+    }
+
+    return (
+        <View className="flex-1 bg-green-500 items-center justify-center p-8">
+            <StatusBar barStyle="light-content" />
+
+            <Image 
+                source={require("@/assets/logo.png")} 
+                className="h-16" 
+                resizeMode="contain" 
+            />
+            <View className="w-full mt-12 gap-3">
+                <Input>
+                    <MaterialCommunityIcons 
+                        name="ticket-confirmation-outline" 
+                        size={20}
+                        color={colors.green[200]}
+                    />
+                    <Input.Field placeholder="Código do ingresso" onChangeText={setCode}  />
+                </Input>
+
+                <Button title="Acessar Credencial" onPress={handleAccessCredential} />
+
+                <Link href='/register' className="text-gray-100 text-base font-bold text-center mt-8">
+                    Ainda não possui ingresso?
+                </Link>
+            </View>
+        </View>
+    )
+}
+
+
+
+
+
+// const styles = StyleSheet.create({
+//     container: {
+//         backgroundColor: "red",
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//     },
+//     title: {
+//         fontSize: 24,
+//         color: "white",
+//     },
+// })
